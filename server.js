56cc8df6201182ac3f8aaf1cd8fea7aa3e567def
JasperStam <jasper@strnk.nl>
2016-04-07 22:03:07 +0200

    Add claiming

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-// var targetFile = 'index.html';
+var _ = require('lodash');
 var nfcDbHandle = 'nfc_db.json';
 var claimDbHandle = 'claim_db.json';
+var tagDbHandle = 'tags_db.json';
 var port = 3000;
 var host = '127.0.0.1';
-var tags = [];
+var currentId = '';
 
 var server = http.createServer( function(req, res) {
     if (req.method === 'POST') {
@@ -26,7 +27,7 @@ var server = http.createServer( function(req, res) {
     }
 });
 
-function createFile(db) {
+var createFile = function(db) {
     var contents = '[]';
     fs.writeFile(db, contents, function(err) {
         if (err) {
@@ -64,28 +65,66 @@ var route = function(body) {
 
 var parseNfcPost = function (request) {
     var nfcDb = parseDb(nfcDbHandle);
+    var tagDb = parseDb(tagDbHandle);
+
     nfcDb.push({
         source: request.source,
         action: request.action,
         timestamp: new Date().toISOString(),
     });
 
+    if (request.action === 'in') {
+        currentId = request.source;
+    }
+
+    if (!getTag(tagDb, request.source)) {
+        tagDb.push({
+            id: request.source,
+            ticket: '',
+        });
+        console.log('pushing to tagDb');
+    }
+
     fs.writeFile(nfcDbHandle, JSON.stringify(nfcDb, null, 4), function(err) {
+        if (err) {
+          console.log(err);
+        }
+    });
+    fs.writeFile(tagDbHandle, JSON.stringify(tagDb, null, 4), function(err) {
         if (err) {
           console.log(err);
         }
     });
 }
 
+var getTag = function(tagDb, tag) {
+    return _.first(_.where(tagDb, {id: tag}));
+};
+
 var parseClaimPost = function (request) {
     var claimDb = parseDb(claimDbHandle);
+    var tagDb = parseDb(tagDbHandle);
+
     claimDb.push({
         action: request.action,
         ticket: request.ticket,
         timestamp: new Date().toISOString(),
     });
 
+    var tagIndex = _.findIndex(tagDb, {id: currentId});
+
+    if (tagIndex < 0) {
+        console.log('Claim before checking', request.ticket);
+    } else {
+        tagDb[tagIndex].ticket = request.ticket;
+    }
+
     fs.writeFile(claimDbHandle, JSON.stringify(claimDb, null, 4), function(err) {
+        if (err) {
+          console.log(err);
+        }
+    });
+    fs.writeFile(tagDbHandle, JSON.stringify(tagDb, null, 4), function(err) {
         if (err) {
           console.log(err);
         }
