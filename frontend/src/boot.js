0641957a94109af7b71df2b5443d450d4fd8ce87
indri-indri <rutger@codeyellow.nl>
2016-04-18 14:09:32 +0200

    Add reset stylesheet

--- frontend/src/boot.js
+++ frontend/src/boot.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'container/App';
 import 'moment-duration-format';
+import './reset.css';
 
 ReactDOM.render(
   <App />,
