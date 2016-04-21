b40d56ad90270d375419013c460098b16c618e4b
Kees Kluskens <kees@webduck.nl>
2016-04-21 21:48:40 +0200

    Hack to get in progress duration working

--- frontend/src/normalize.js
+++ frontend/src/normalize.js
@@ -3,10 +3,11 @@ import moment from 'moment';
 export function normalizeActivity(item) {
     const startedAt = moment.unix(item.started_at);
     const endedAt = item.ended_at ? moment.unix(item.ended_at) : null;
+    const ended = endedAt || moment.unix(item.now);
     return {
         ...item,
         started_at: startedAt,
         ended_at: endedAt,
-        started_ended_diff: endedAt ? moment.duration(endedAt.diff(startedAt)) : null,
+        started_ended_diff: moment.duration(ended.diff(startedAt)),
     };
 }
