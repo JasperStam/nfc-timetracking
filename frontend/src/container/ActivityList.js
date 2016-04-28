53b0fcf0a0dadaae8e43953108b99d0283b23b6e
Kees Kluskens <kees@webduck.nl>
2016-04-28 09:18:03 +0200

    fix linting

--- frontend/src/container/ActivityList.js
+++ frontend/src/container/ActivityList.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ActivityList from 'component/ActivityList';
 import axios from 'axios';
 import { normalizeActivity } from 'normalize';
-// import _ from 'lodash';
 
 const REFRESH_INTERVAL = 1000;
 
@@ -27,7 +26,7 @@ export default React.createClass({
     },
     saveActivity(id, data) {
         return axios.patch(`${MODUS_CONFIG.apiUrl}/activity/${id}`, data)
-        .then((payload) => {
+        .then(() => {
             // TODO: Not necessary for now because we refresh every sec.
             // const activityIndex = _.findIndex(this.state.activities, { id });
             // const activities = _.clone(this.state.activities);
