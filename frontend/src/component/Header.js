974bf14efca7237aee1ea83a6d8ad5ab05a6993b
indri-indri <rutger@codeyellow.nl>
2016-05-20 14:02:51 +0200

    Add faux navigation

--- frontend/src/component/Header.js
+++ frontend/src/component/Header.js
@@ -5,7 +5,13 @@ export default React.createClass({
     render() {
         return (
             <div className={styles.container}>
-                <h1 className={styles.header}>Modus</h1>
+                <div className={styles.navigation}>
+                    <div className={styles.selected}>
+                        <a href="#" className={styles.button}>Activities</a>
+                    </div>
+                    <a href="#" className={styles.button}>Insights</a>
+                </div>
+                <a className={styles.button} href="#">Sign out</a>
             </div>
         );
     },
